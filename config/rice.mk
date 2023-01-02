08becb24ea20909545b062df2dde640690a7403e
Alvin Francis <nivlafx@gmail.com>
2023-01-02 01:51:55 -0400

    More rice changes
    Change-Id: I9f6188bb240417747391cfa712f00fe499cacc30

---
+++ config/rice.mk
@@ -0,0 +1,101 @@
+# Additional props
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.com.google.ime.theme_id=5 \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true \
+    persist.sys.disable_rescue=true \
+    ro.opa.eligible_device=true
+
+# Setupwizard
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.esim_cid_ignore=00000001 \
+    ro.setupwizard.rotation_locked=true \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.lifecycle_refactoring=true \
+    setupwizard.feature.notification_refactoring=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.theme=glif_v3_light
+
+# Media
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.recorder.show_manufacturer_and_model=true
+
+# Rice properties - used by firmware version
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.rice.maintainer=$(RICE_MAINTAINER) \
+    ro.rice.code=$(RICE_CODENAME) \
+    ro.riseui.code=$(RISE_CODE) \
+    ro.rice.packagetype=$(RICE_PACKAGE_TYPE) \
+    ro.rice.releasetype=$(RICE_BUILDTYPE) \
+    ro.rice.version?=$(RICE_VERSION) \
+    ro.rice.chipset?=$(RICE_CHIPSET) \
+    ro.rice.build.version=$(LINEAGE_VERSION) \
+    ro.rice.display.version?=$(LINEAGE_DISPLAY_VERSION) \
+    ro.rice.platform_release_codename=$(RICE_FLAVOR) \
+    ro.ricelegal.url=https://www.manginasal.com/
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
+# Parallel space
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/privapp_whitelist_ink.kscope.parallelspace.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_ink.kscope.parallelspace.xml
+
+# Blurs
+ifeq ($(TARGET_ENABLE_BLUR), true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.launcher.blur.appLaunch=0
+endif
+
+TARGET_BUILD_APERTURE_CAMERA ?= false
+ifeq ($(strip $(TARGET_BUILD_APERTURE_CAMERA)),true)
+PRODUCT_PACKAGES += \
+    Aperture
+endif
+
+# Bootanimation
+ifeq ($(SUSHI_BOOTANIMATION),720)
+# Sushi Bootanimation 720 by saavedra3
+PRODUCT_COPY_FILES += vendor/lineage/bootanimation/sushi_bootanimation_720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(SUSHI_BOOTANIMATION),1080)
+# Sushi Bootanimation 1080 by saavedra3
+PRODUCT_COPY_FILES += vendor/lineage/bootanimation/sushi_bootanimation_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(SUSHI_BOOTANIMATION),1440)
+# Sushi Bootanimation 1440 by saavedra3
+PRODUCT_COPY_FILES += vendor/lineage/bootanimation/sushi_bootanimation_1440.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else
+PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+endif
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    Backgrounds \
+    GameSpace \
+    RepainterServicePriv \
+    ParallelSpacePrebuilt \
+    OmniJaws
+
+# Spoof fingerprint for Google Play Services and SafetyNet
+ifeq ($(PRODUCT_OVERRIDE_GMS_FINGERPRINT),)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=google/raven/raven:13/TQ1A.221205.011/9244662:user/release-keys
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=$(PRODUCT_OVERRIDE_GMS_FINGERPRINT)
+endif
